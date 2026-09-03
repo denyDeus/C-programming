678a9668391d8ca964283a2d542ec0cbe2ae44bd
Denis Deus <deusdenis40@gmail.com>
2026-09-03 21:56:34 +0300

    Add C comment examples
    Add sample C programs demonstrating single-line and multi-line comments. The examples show how comments are ignored by the compiler and how they can be used to annotate code.

---
+++ C Multi-line comments.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	/* The code below will print the words Helllo World!
+	to the screen, and it is amaizing */
+	printf("Hello Denis!");
+}
---
+++ C comments.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	printf("This will run");
+	// printf("This won't run");
+	return 0;
+}
