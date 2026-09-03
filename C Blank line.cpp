b98d0941dc26f5b512c17bd8db96726a249f39f4
Denis Deus <deusdenis40@gmail.com>
2026-09-03 21:35:34 +0300

    Add beginner C output examples
    Added a few small C programs demonstrating different output formatting: no newline, newline after each line, and blank-line spacing. Also included the corresponding compiled Windows executables for quick execution.

---
+++ C Blank line.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello Denis!\n\n");
+	printf("I am learning C.\n\n");
+	printf("And it is awesome!");
+	return 0;
+}
---
+++ C output.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello Denis!");
+	printf("I am learning C.");
+	printf("And it is awesome!");
+	return 0;
+}
